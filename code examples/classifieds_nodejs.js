baa407c08018c1ea12e2e5849b70da422b2097cd
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:34:29 +0200

    Adding code examples/classifieds_nodejs.js

---
+++ code examples/classifieds_nodejs.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const body = {
+  'source': 'universal',
+  'url': 'https://newyork.craigslist.org/search/ata#search=1~gallery~0~0'};
+  
+const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+  }
+});
+
+console.log(await response.json());
