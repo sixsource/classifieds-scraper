3a1c53529a8166faacbb25775c522e21e32f2133
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:34:30 +0200

    Adding code examples/classifieds_curl.sh

---
+++ code examples/classifieds_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://newyork.craigslist.org/search/ata#search=1~gallery~0~0"}'
